7a1869cb61ed2911d20cdf4f634fe65f21758459
rouxxi <laskar.gwenvael@gmail.com>
2025-08-14 13:04:40 +0200

    feat: api PORT is now editable

--- config.ts
+++ config.ts
@@ -4,5 +4,5 @@ dotenv.config();
 export const ENV = {
     DATABASE_KEY: process.env.DATABASE_KEY,
     DATABASE_URL: process.env.DATABASE_URL,
-    API_PORT: process.env.API_PORT || "8080",
+    API_PORT: process.env.API_PORT || 8080,
 }
